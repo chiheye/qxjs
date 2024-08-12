52adc93035903331feea2dc74853a60d7aecec64
chiheye <42902754+chiheye@users.noreply.github.com>
2024-08-12 09:20:44 +0800

    Add files via upload

---
+++ get_arcfoxcookie.js
@@ -0,0 +1,24 @@
+[rewrite_local]
+# 拦截指定请求并提取Cookie
+https://mg.arcfox.cn/user/public/account/getUserInfo url script-response-header https://raw.githubusercontent.com/chiheye/qxjs/main/get_arcfoxcookie.js
+
+[mitm]
+hostname=mg.arcfox.cn
+
+
+
+
+let url = $request.url;
+let method = $request.method;
+let headers = $request.headers;
+
+if (url.includes("mg.arcfox.cn/user/public/account/getUserInfo") && method === "GET") {
+  let cookie = headers["Cookie"];
+  let user_token = headers["token"];
+
+  if (cookie) {
+    $prefs.setValueForKey(cookie, "arcfox_cookie");
+    $prefs.setValueForKey(user_token, "arcfox_token");
+    $notify("Cookie获取成功", "Cookie和Token已成功保存", "");
+  }
+  $done();
