b225577599748110ab04cc69bed73be60d84731d
chiheye <42902754+chiheye@users.noreply.github.com>
2024-08-13 09:57:42 +0800

    Update arcfoxqd.js

--- arcfoxqd.js
+++ arcfoxqd.js
@@ -9,7 +9,7 @@ QuantumultX 远程脚本配置:
 [rewrite_local]
 # 获取arcfoxCookie
 
-^https:\/\/mg\.arcfox\.cn\/mall-integral url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/arcfoxqd.js
+https://mg.arcfox.cn/mall-integral/public/integral/getUserIntegral url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/arcfoxqd.js
 
 [mitm] 
 
