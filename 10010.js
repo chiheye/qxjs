280d9834ac84024c662c42449ed008a1c3bf41d9
chiheye <42902754+chiheye@users.noreply.github.com>
2024-08-13 11:48:57 +0800

    Create 10010.js

---
+++ 10010.js
@@ -0,0 +1,14 @@
+/*
+吾爱破解签到脚本
+************************
+QuantumultX 远程脚本配置:
+************************
+
+
+    
+[rewrite_local]
+# 获取10010Cookie
+https:\/\/www\.52pojie\.cn\/home\.php\? url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/10010.js
+
+[mitm] 
+hostname= activity.10010.com
