3d5084c7a78ec0d7d07f36e50b9b248d53f01fa2
chiheye <42902754+chiheye@users.noreply.github.com>
2024-08-13 14:24:13 +0800

    Update 10010.js

--- 10010.js
+++ 10010.js
@@ -9,6 +9,8 @@ QuantumultX 远程脚本配置:
 [rewrite_local]
 # 获取10010Cookie
 ^https:\/\/activity\.10010\.com\/sixPalaceGridTurntableLottery\/signin\/daySign url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/10010.js
+^https:\/\/activity\.10010\.com\/sixPalaceGridTurntableLottery\/task\/taskList\?type\=1 url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/10010.js
+
 
 [mitm] 
 hostname= activity.10010.com
