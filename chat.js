c56e6b127769625251bd0602658fb590fb101467
chiheye <42902754+chiheye@users.noreply.github.com>
2024-08-14 13:57:20 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,3 +1,10 @@
-var obj = JSON.parse($response.body);
-obj.payload.plan = "premium";
-$done({ body: JSON.stringify(obj) });
+const url = 'https://www.typingmind.com/api/license';
+
+// 修改响应内容
+if ($request.url.indexOf(url) != -1) {
+  let body = JSON.parse($response.body);
+  body.payload.plan = 'premium';
+  $done({body: JSON.stringify(body)});
+} else {
+  $done({});
+}
