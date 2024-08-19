e6096320e1109cbe734e26dee267def12808d90c
chiheye <42902754+chiheye@users.noreply.github.com>
2024-08-19 15:08:32 +0800

    Update arcfoxqd.js

--- arcfoxqd.js
+++ arcfoxqd.js
@@ -6,10 +6,30 @@ QuantumultX 远程脚本配置:
 [rewrite_local]
 # 获取arcfoxCookie
 //^https:\/\/mg\.arcfox\.cn\/arcfox-brand\/public\/buriedPoint? url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/arcfoxqd.js
-^https:\/\/mg\.arcfox\.cn url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/arcfoxqd.js
+^https:\/\/mg\.arcfox\.cn\/update-center\/checkVersion? url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/arcfoxqd.js
 [mitm] 
 hostname= mg.arcfox.cn
 ************************************/
 
 
+if ($request && $request.headers) {
+    // 打印完整的请求头
+    console.log('请求头信息：' + JSON.stringify($request.headers));
+    
+    // 尝试获取并保存Cookie
+    GetCookie();
+    $done({});
+}
+
+function GetCookie() {
+    const cookie = $request.headers['Cookie'] || $request.headers['cookie'];
+    if (cookie) {
+        $prefs.setValueForKey(cookie, 'arcfox_cookie');
+        console.log('Cookie保存成功：' + cookie);
+        $notify("极狐", "Cookie保存成功", "");
+    } else {
+        console.log('获取Cookie失败');
+        $notify("极狐", "Cookie获取失败", "未能获取到Cookie，请检查设置。");
+    }
+}
 
