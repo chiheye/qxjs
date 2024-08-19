7ad97fe35b847a98c2e20cd2bb7bd3e218b9ac5f
chiheye <42902754+chiheye@users.noreply.github.com>
2024-08-19 13:39:02 +0800

    Update arcfoxqd.js

--- arcfoxqd.js
+++ arcfoxqd.js
@@ -5,8 +5,8 @@ QuantumultX 远程脚本配置:
 ************************  
 [rewrite_local]
 # 获取arcfoxCookie
-^https:\/\/mg\.arcfox\.cn\/arcfox-brand\/public\/buriedPoint? url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/arcfoxqd.js
-
+//^https:\/\/mg\.arcfox\.cn\/arcfox-brand\/public\/buriedPoint? url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/arcfoxqd.js
+^https:\/\/mg\.arcfox\.cn url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/arcfoxqd.js
 [mitm] 
 hostname= mg.arcfox.cn
 ************************************/
