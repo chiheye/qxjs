e6eec44c11111d187a2541213043f4523d8d5613
chiheye <42902754+chiheye@users.noreply.github.com>
2024-08-19 13:56:48 +0800

    Update arcfoxqd.js

--- arcfoxqd.js
+++ arcfoxqd.js
@@ -11,28 +11,5 @@ QuantumultX 远程脚本配置:
 hostname= mg.arcfox.cn
 ************************************/
 
-// 这个脚本将捕获所有响应头并提取Set-Cookie
 
-let cookie = "";
-let cookieCaptured = false;
-
-// 捕获响应头中的Set-Cookie
-$network.addEventListener("response", (event) => {
-  const { response } = event;
-  const cookies = response.headers["Set-Cookie"] || [];
-  cookies.forEach((item) => {
-    if (item.includes(cookieKey)) {
-      cookie = item.split(";")[0];
-      cookieCaptured = true;
-      console.log("Captured cookie:", cookie);
-    }
-  });
-
-  // 提示Cookie获取失败
-  if (!cookieCaptured) {
-    console.log("Failed to capture cookie");
-  }
-});
-
-$done({});  // 完成处理
 
