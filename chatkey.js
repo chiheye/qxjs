89a269935842276d81c6c5438f6e789ccc27b43c
chiheye <42902754+chiheye@users.noreply.github.com>
2024-08-28 23:53:19 +0800

    Update chatkey.js

--- chatkey.js
+++ chatkey.js
@@ -8,17 +8,14 @@ hostname = api-v4-generatechatstream-7hb5gcrmka-uc.a.run.app
 */
 
 
-
-// 监听请求头
 const url = `^https:\/\/api-v4-generatechatstream-7hb5gcrmka-uc\.a\.run\.app`;
 
 if ($request && $request.url.match(url)) {
     const authorizationHeader = $request.headers['authorization'];
-    const bearerToken = authorizationHeader.split(' ')[1]; // 提取Bearer token
+    const bearerToken = authorizationHeader.split(' ')[1]; 
 
     if (bearerToken) {
         console.log(`Bearer Token: ${bearerToken}`);
-        // 发送 HTTP 请求到本地 API 服务以保存 Token
         const saveTokenRequest = {
             url: 'http://fastchat.dynv6.net:8300/save-token',
             method: 'POST',
