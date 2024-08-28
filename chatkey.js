d9ca8a1f23a6ece7f01b4047826e1493a1c311ac
chiheye <42902754+chiheye@users.noreply.github.com>
2024-08-29 00:22:04 +0800

    Update chatkey.js

--- chatkey.js
+++ chatkey.js
@@ -2,7 +2,7 @@
 
 /**
 [rewrite_local]
-https:\\api-v4-generatechatstream-7hb5gcrmka-uc.a.run.app url script-request-header ⁨https://raw.githubusercontent.com/chiheye/qxjs/main/chatkey.js
+https:\\*run.app url script-request-header ⁨https://raw.githubusercontent.com/chiheye/qxjs/main/chatkey.js
 [mitm]  
 https:\\api-v4-generatechatstream-7hb5gcrmka-uc.a.run.app
  **/
