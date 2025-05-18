58de08609f22ed8640186233df3d349a52ef48d4
chiheye <42902754+chiheye@users.noreply.github.com>
2025-05-19 07:19:29 +0800

    更新 goodnotes_rewrite.js

--- goodnotes_rewrite.js
+++ goodnotes_rewrite.js
@@ -1,6 +1,15 @@
- ﻿[rewrite_local]﻿ 
-^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$ url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/goodnotes_rewrite.js
+/*
+2025.5.19 23.15
 
+
+
+[rewrite_local]﻿ 
+^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$ url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/goodnotes_rewrite.js
+[MITM]
+hostname = %APPEND% isi.csan.goodnotes.com.*, isi.csan.goodnotes.com
+
+
+*/
 // GoodNotes 请求重写脚本
 var modifiedUrl = "https://goodenotes6.lovebabyforever.workers.dev/";
 var headers = $request.headers;
