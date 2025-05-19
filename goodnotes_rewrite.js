bcc3a8c071d8dedbd6334b4d5b1e55e36ce0026a
chiheye <42902754+chiheye@users.noreply.github.com>
2025-05-19 08:06:51 +0800

    更新 goodnotes_rewrite.js

--- goodnotes_rewrite.js
+++ goodnotes_rewrite.js
@@ -6,8 +6,8 @@
 [rewrite_local]﻿ 
 
 
-^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$ script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/
-^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$ script-request-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/
+^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$ script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/goodnotes_rewrite.js
+^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$ script-request-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/goodnotes_rewrite.js
 
 [MITM]
 hostname = %APPEND% isi.csan.goodnotes.com.*, isi.csan.goodnotes.com
