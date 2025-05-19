3bdd6df2dc0bb04a51ca2a1519faacbdffc5889f
chiheye <42902754+chiheye@users.noreply.github.com>
2025-05-19 08:05:30 +0800

    更新 goodnotes_rewrite.js

--- goodnotes_rewrite.js
+++ goodnotes_rewrite.js
@@ -4,7 +4,11 @@
 
 
 [rewrite_local]﻿ 
-^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$ url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/goodnotes_rewrite.js
+
+
+^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$ script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/
+^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$ script-request-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/
+
 [MITM]
 hostname = %APPEND% isi.csan.goodnotes.com.*, isi.csan.goodnotes.com
 
@@ -13,12 +17,26 @@ hostname = %APPEND% isi.csan.goodnotes.com.*, isi.csan.goodnotes.com
 
 
 
-// goodnotes_host_rewrite.js
-let url = $request.url.replace(
-    /^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/([a-f0-9\-]{36})$/,
-    "https://goodenotes6.lovebabyforever.workers.dev/v1/subscribers/$1"
-);
-let headers = Object.assign({}, $request.headers, {
-    Host: "goodenotes6.lovebabyforever.workers.dev"
-});
-$done({ url, headers });
+// goodnotes_rewrite.js
+const url = $request.url;
+const headers = $request.headers;
+const targetHost = 'goodenotes6.lovebabyforever.workers.dev';
+const targetUrl = 'https://goodenotes6.lovebabyforever.workers.dev/';
+
+// 检查是否匹配目标 URL
+if (/^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$/.test(url)) {
+    // 重写 URL
+    let newUrl = targetUrl;
+    
+    // 重写 Host 头部
+    headers['Host'] = targetHost;
+    
+    // 返回修改后的请求
+    $done({
+        url: newUrl,
+        headers: headers
+    });
+} else {
+    // 不匹配，直接返回原请求
+    $done({});
+}
