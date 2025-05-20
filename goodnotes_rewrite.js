56f6853f2cfea5d3172b4dfcc687c0b70239db3f
chiheye <42902754+chiheye@users.noreply.github.com>
2025-05-20 10:44:06 +0800

    更新 goodnotes_rewrite.js

--- goodnotes_rewrite.js
+++ goodnotes_rewrite.js
@@ -7,18 +7,18 @@ Goodnotes 订阅请求重写脚本
 ^https:\/\/goodenotes6\.lovebabyforever\.workers\.dev\/ url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/goodnotes_headers.js
 
 [MITM]
-hostname = %APPEND% isi.csan.goodnotes.com
+hostname = %APPEND% isi.csan.goodnotes.com,goodenotes6.lovebabyforever.workers.dev
+
+
 */
 
-const originalUrl = $request.url;
 const originalHeaders = $request.headers;
 const targetHost = 'goodenotes6.lovebabyforever.workers.dev';
 
-// 构建新 URL
-const url = originalUrl.replace(/^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$/, `https://${targetHost}`);
+
 
 // 修改 Host 头
 originalHeaders['Host'] = targetHost;
 
 // 返回修改后的请求
-$done({ url, headers: originalHeaders });
+$done({ headers: originalHeaders });
