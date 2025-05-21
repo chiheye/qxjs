6911b0958ecff49d1377e89ad205d7ba96033020
chiheye <42902754+chiheye@users.noreply.github.com>
2025-05-21 08:16:03 +0800

    更新 goodnotes_rewrite.js

--- goodnotes_rewrite.js
+++ goodnotes_rewrite.js
@@ -3,7 +3,6 @@ Goodnotes 订阅请求重写脚本
 最后更新：2025.5.19 23:15
 
 [rewrite_local]
-^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$ url 302 https://goodenotes6.lovebabyforever.workers.dev
 ^https:\/\/goodenotes6\.lovebabyforever\.workers\.dev\/ url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/goodnotes_headers.js
 
 [MITM]
@@ -14,7 +13,25 @@ hostname = %APPEND% isi.csan.goodnotes.com,goodenotes6.lovebabyforever.workers.d
 
 const originalHeaders = $request.headers;
 const targetHost = 'goodenotes6.lovebabyforever.workers.dev';
+// const originalUrl = $request.url;
 
+// Quantumult X Rewrite Script
+// 用于重定向特定的 Goodnotes 请求
+
+// 定义需要匹配的 URL 正则表达式
+const urlPattern = /^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$/;
+
+// 检查当前请求的 URL 是否匹配模式
+if (urlPattern.test($request.url)) {
+    // 如果匹配，将请求重定向到新的 URL
+    $done({ url: "https://goodenotes6.lovebabyforever.workers.dev/" });
+} else {
+    // 如果不匹配，继续处理原始请求
+    $done({});
+}
+
+//修改请求的Url
+//const url = originalUrl.replace(/^https:\/\/isi\.csan\.goodnotes.*\/v1\/subscribers\/[a-f0-9\-]{36}$/, `https://${targetHost}`);
 
 
 // 修改 Host 头
