7f4f3071aafbef2e7b6a5ea1fc188bbf23e2ecc0
chiheye <42902754+chiheye@users.noreply.github.com>
2025-06-22 20:15:13 +0800

    update

--- kkmh.js
+++ kkmh.js
@@ -15,7 +15,7 @@
 ^https?:\/\/m.kuaikanmanhua.com\/v1\/vip\/me url script-response-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmh.js
 ^https?:\/\/.*kkmh.com\/v2\/comic\/detail\/get url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmh.js
 ^https?:\/\/m.kuaikanmanhua.com\/v2\/comic\/detail\/get url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmh.js
-^https:\/\/m\.kuaikanmanhua\.com\/v2\/kb\/mini\/wallet\? url script-response-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmhkkb.js
+^https:\/\/m\.kuaikanmanhua\.com\/v2\/kb\/mini\/wallet url script-response-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmhkkb.js
 ^https:\/\/.*kkmh.com\/v2\/kb\/mini\/wallet\? url script-response-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmhkkb.js
 ^https:\/\/m\.kuaikanmanhua\.com\/v2\/kb\/point\/charge_h5\? url script-response-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmhch5.js
 ^https:\/\/.*kkmh.com\/v2\/kb\/point\/charge_h5\? url script-response-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmhch5.js
@@ -24,7 +24,7 @@
 ^https:\/\/m\.kuaikanmanhua\.com\/v2\/comicbuy\/encrypt_buy_h5\? url script-response-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmhcb.js
 ^https:\/\/.*kkmh.com\/v2\/comicbuy\/encrypt_buy_h5\? url script-response-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmhcb.js
 ^https:\/\/m\.kuaikanmanhua\.com\/v2\/passport\/user\/me\? url script-response-body url script-response-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmhu.js
-^https:\/\/.*kkmh.com\/v2\/passport\/user\/me\? url script-response-body url script-response-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmhu.js
+^https:\/\/.*kkmh.com\/v2\/passport\/user\/me\? url script-response-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/kkmhu.js
 
 # >去广告
 ^https?://.*kkmh.com\/v3\/ad\/show url reject-dict
