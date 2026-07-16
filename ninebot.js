fa8d369893a38ad86eb0dc31ba63c1159afcfc31
chiheye <42902754+chiheye@users.noreply.github.com>
2026-07-17 07:27:16 +0800

    Update ninebot.js

--- ninebot.js
+++ ninebot.js
@@ -7,7 +7,7 @@
  * * === Quantumult X 配置 ===
  * * [rewrite_local]
  * # 抓取九号出行签到 Token
- * ^https:\/\/cn-cbu-gateway\.ninebot\.com\/portal\/api\/user-sign\/.* url script-request-header ninebot.js
+ * ^https:\/\/cn-cbu-gateway\.ninebot\.com\/portal\/api\/user-sign\/.* url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/ninebot.js
  * * [task_local]
  * # 每日自动签到（每天上午 8:00 执行）
  * 0 7 * * * ninebot.js, tag=九号出行签到, img-url=https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Car.png, enabled=true
