de8dd29fcc895008622bdf5535680f7d8eef0ff7
chiheye <42902754+chiheye@users.noreply.github.com>
2026-08-13 07:12:05 +0800

    Update ithome.js

--- ithome.js
+++ ithome.js
@@ -13,7 +13,7 @@ hostname = napi.ithome.com
 [rewrite_local]
 # 获取 Token（第一次使用，获取后可禁用）
 # 触发方式：在 IT之家 App 内打开签到页面即可触发
-^https?://(napi\.ithome\.com|my\.ruanmei\.com)/ api/ url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/ithome.js
+^https?://(napi\.ithome\.com|my\.ruanmei\.com)/api/ url script-request-header https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/ithome.js
 
 # 执行签到（可保留）
 ^https?://napi\.ithome\.com/api/usersign/sign url script-response-body https://raw.githubusercontent.com/chiheye/qxjs/refs/heads/main/ithome.js
